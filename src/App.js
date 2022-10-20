32636a2c300bac220d81f4d72518e69a8772a84e
Radwa Mamdouh <rmamdouh7979@gmail.com>
2022-10-20 13:22:27 +0200

    handle project structure and colors vars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import PrimeReact from "primereact/api";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
+
+// STYLES
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	PrimeReact.ripple = true;
+
+	return (
+		<div className="App">
+			<div>header</div>
+			<main>routes</main>
+			<div>footer</div>
+		</div>
+	);
 }
 
 export default App;
